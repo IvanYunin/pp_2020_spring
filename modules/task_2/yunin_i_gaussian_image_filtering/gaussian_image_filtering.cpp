40d2e1e1451009917bf25080b4183a89e6501a4d
IvanYunin <79103889939@yandex.ru>
2020-05-16 05:54:46 +0300

    task123

---
+++ modules/task_2/yunin_i_gaussian_image_filtering/gaussian_image_filtering.cpp
@@ -0,0 +1,62 @@
+// Copyright 2020 Ivan Yunin
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <numeric>
+#include <random>
+#include <stdexcept>
+#include "../../modules/task_2/yunin_i_gaussian_image_filtering/gaussian_image_filtering.h"
+
+
+static int offset = 0;
+static const double filter[9] = { 1.0/16, 1.0/8, 1.0/16,
+    1.0/8, 1.0/4, 1.0/8,
+    1.0/16, 1.0/8, 1.0/16 };
+
+inline int get_left_border(int curr_coord, int min_coord) {
+    if (curr_coord - 1 < 0)
+        return 0;
+    else
+        return -1;
+}
+
+inline int get_right_border(int curr_coord, int max_coord) {
+    if (curr_coord - max_coord == -1)
+        return 0;
+    else
+        return 1;
+}
+
+void filter_on_line(uint8_t* img, uint8_t* res, int cols_num, int rows_num, int curr_row) {
+    for (int i = 0; i < cols_num; ++i) {
+        double curr_pixel = 0;
+        int x_left = get_left_border(i, 0);
+        int y_left = get_left_border(curr_row, 0);
+        int x_right = get_right_border(i, cols_num);
+        int y_right = get_right_border(curr_row, rows_num);
+
+        for (int x = x_left; x <= x_right; ++x) {
+            for (int y = y_left; y <= y_right; ++y) {
+                curr_pixel += filter[x+1 + (y+1)*3]*img[i+x  + (y)*cols_num];
+            }
+        }
+        res[i] = static_cast<uint8_t>(curr_pixel);
+    }
+}
+
+
+void apply_filter(uint8_t* img, uint8_t* res, int cols_num, int rows_num) {
+    #pragma omp parallel for
+    for (int i = 0; i < rows_num; ++i) {
+        filter_on_line(&img[i*cols_num], &res[i*cols_num], cols_num, rows_num, i);
+    }
+}
+
+void generate_random_image(uint8_t* img, int rows, int cols) {
+    std::mt19937 gen;
+    gen.seed((unsigned)time(0) + ++offset);
+    std::uniform_int_distribution<int> dis(0, 255);
+    for (int i = 0; i < rows * cols; ++i) {
+        img[i] = static_cast<uint8_t>(dis(gen));
+    }
+}
---
+++ modules/task_3/yunin_i_gaussian_image_filtering/main.cpp
@@ -0,0 +1,64 @@
+// Copyright 2020 Obolenskiy Arseniy
+#include <gtest/gtest.h>
+#include <omp.h>
+#include "./gaussian_image_filtering.h"
+
+TEST(Gaussian_Image_Filtering_seq, Can_Filter_Small_Image) {
+    const int width = 10;
+    const int height = 10;
+    uint8_t* img = new uint8_t(width*height);
+    uint8_t* res = new uint8_t(width*height);
+    generate_random_image(img , width, height);
+    apply_filter(img, res, width, height);
+    for (int i = 0; i < width * height; ++i) {
+        ASSERT_NE(res[i], img[i]);
+    }
+}
+
+TEST(Gaussian_Image_Filtering_seq, Can_Handle_Images_With_Width_Less_Than_Kernel_Size) {
+    const int width = 2;
+    const int height = 50;
+
+    uint8_t* img = new uint8_t(width*height);
+    uint8_t* res = new uint8_t(width*height);
+    generate_random_image(img , width, height);
+    apply_filter(img, res, width, height);
+    for (int i = 0; i < width * height; ++i) {
+        ASSERT_NE(res[i], img[i]);
+    }
+}
+
+TEST(Gaussian_Image_Filtering_seq, Can_Handle_Images_With_Height_Less_Than_Kernel_Size) {
+    const int width = 50;
+    const int height = 2;
+
+    uint8_t* img = new uint8_t(width*height);
+    uint8_t* res = new uint8_t(width*height);
+    generate_random_image(img , width, height);
+    apply_filter(img, res, width, height);
+    for (int i = 0; i < width * height; ++i) {
+        ASSERT_NE(res[i], img[i]);
+    }
+}
+
+TEST(Gaussian_Image_Filtering_seq, is_correct_for3x3) {
+    const int width = 3;
+    const int height = 3;
+
+    uint8_t* img = new uint8_t(width*height);
+    uint8_t* res = new uint8_t(width*height);
+    uint8_t ans[9] = { 15, 31, 15, 31, 63, 31, 15, 31, 15};
+    for (int i = 0; i < width * height; ++i) {
+        img[i] = 0;
+    }
+    img[4] = 255;
+    apply_filter(img, res, width, height);
+    for (int i = 0; i < width * height; ++i) {
+        ASSERT_EQ(res[i], ans[i]);
+    }
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
