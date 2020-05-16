30b29eb846c81f6bf11d4bce29a2123aff93dfb5
IvanYunin <79103889939@yandex.ru>
2020-05-16 06:10:53 +0300

    change_test_name

---
+++ modules/task_3/yunin_i_gaussian_image_filtering/gaussian_image_filtering.h
@@ -0,0 +1,13 @@
+// Copyright 2020 Ivan Yunin
+#ifndef MODULES_TASK_3_YUNIN_I_GAUSSIAN_IMAGE_FILTERING_GAUSSIAN_IMAGE_FILTERING_H_
+#define MODULES_TASK_3_YUNIN_I_GAUSSIAN_IMAGE_FILTERING_GAUSSIAN_IMAGE_FILTERING_H_
+#include <cstdint>
+#include <vector>
+
+inline int get_left_border(int, int);
+inline int get_right_border(int, int);
+void filter_on_line(double*, uint8_t*, uint8_t*, int, int, int);
+void generate_random_image(uint8_t*, int, int);
+void apply_filter(uint8_t*, uint8_t*, int, int);
+
+#endif  // MODULES_TASK_3_YUNIN_I_GAUSSIAN_IMAGE_FILTERING_GAUSSIAN_IMAGE_FILTERING_H_
